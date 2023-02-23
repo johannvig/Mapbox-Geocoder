fe838e7d24f405e305eadad2070f911ab5f58235
Vigouroux Johanne <102874093+johannvig@users.noreply.github.com>
2023-02-23 18:35:21 +0100

    Update geocoder.py

--- geocoder.py
+++ geocoder.py
@@ -2,44 +2,45 @@ import requests.auth
 import warnings
 from colorama import Fore
 from mapbox import Geocoder
+
 warnings.filterwarnings('ignore')
 
 
-token=input("Write here your mapbox API")
-latitude=input("What is the latitude of your location?")
-longitude=input("What is the longitude of your location?")
-
-#send a requests to the mapbox API to get multiple addresses close to the starting location
-w = requests.get("https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/"+longitude+","+latitude+".json?radius=1000&limit=50&geometry=polygon&access_token="+token)
-
-
-a=0
-try:
-  while True:
-    #cleaning the request response to have only coordinates
-    response = w.json()['features'][a]['geometry']['coordinates']
-
-    mot1 = str(response).replace('[', ' ')
-    mot2 = str(mot1).replace(']', ' ')
-    
-    #separate longitude and latitude
-    séparer = str(mot2).split(',')
-
-    longitude_x=séparer[0]
-    latitude_y=séparer[1]
+def get_coordinates(longitude, latitude, token):
+    """
+    This function sends a requests to the Mapbox API to get multiple addresses close to the starting location.
+    It returns a list of coordinates as strings.
+    """
+    url = f"https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/{longitude},{latitude}.json?radius=1000&limit=50&geometry=polygon&access_token={token}"
+    response = requests.get(url)
+    response.raise_for_status()
+    coordinates = []
+    for feature in response.json()['features']:
+        coordinates.append(str(feature['geometry']['coordinates']).replace('[', '').replace(']', ''))
+    return coordinates
 
 
-    #convert coordinates into addresses
+def get_address(longitude, latitude, token):
+    """
+    This function converts coordinates into addresses.
+    It returns a list of addresses as strings.
+    """
     geocoder = Geocoder(access_token=token)
-    response = geocoder.reverse(lon=longitude_x, lat=latitude_y, types=['address'])
-
-    #cleaning the request response to have only addresses
-    data = response.json()['features'][0]['place_name']
-    print(data)
+    response = geocoder.reverse(lon=longitude, lat=latitude, types=['address'])
+    response.raise_for_status()
+    return response.json()['features'][0]['place_name']
 
 
-    a+=1
-
-except Exception as e:
+if __name__ == "__main__":
+    token = input("Write here your Mapbox API key: ")
+    latitude = input("What is the latitude of your location? ")
+    longitude = input("What is the longitude of your location? ")
+    try:
+        coordinates = get_coordinates(longitude, latitude, token)
+        for coordinate in coordinates:
+            longitude_x, latitude_y = coordinate.split(',')
+            address = get_address(longitude_x, latitude_y, token)
+            print(address)
+    except Exception as e:
         print(Fore.RED + "Ohoh! Something went wrong!")
         print(e)
